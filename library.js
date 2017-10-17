c9ef96011bac7fe0b150f41447fbd7d179b3c55c
Julian Lam <julian@nodebb.org>
2017-10-17 10:08:40 -0400

    some more personalisation to my fork of quickstart...

--- library.js
+++ library.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
-var controllers = require('./lib/controllers'),
+var controllers = require('./lib/controllers');
 
-	plugin = {};
+var plugin = {};
 
-plugin.init = function(params, callback) {
-	var router = params.router,
-		hostMiddleware = params.middleware,
-		hostControllers = params.controllers;
-		
+plugin.init = function (params, callback) {
+	var router = params.router;
+	var hostMiddleware = params.middleware;
+	var hostControllers = params.controllers;
+
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.
 
@@ -18,11 +18,11 @@ plugin.init = function(params, callback) {
 	callback();
 };
 
-plugin.addAdminNavigation = function(header, callback) {
+plugin.addAdminNavigation = function (header, callback) {
 	header.plugins.push({
 		route: '/plugins/quickstart',
 		icon: 'fa-tint',
-		name: 'Quickstart'
+		name: 'Quickstart',
 	});
 
 	callback(null, header);
