c19792aed20ff88d5b3e828b587d4b53c4d5d50c
Andrew Rodrigues <psycho.bunny@hotmail.com>
2019-05-14 15:59:58 -0400

    feat: added colorpicker example to sample ACP page

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -2,26 +2,42 @@
 
 /* globals $, app, socket, define */
 
-define('admin/plugins/quickstart', ['settings'], function (Settings) {
+define('admin/plugins/quickstart', ['settings', 'admin/modules/colorpicker'], function (settings, colorpicker) {
 	var ACP = {};
 
 	ACP.init = function () {
-		Settings.load('quickstart', $('.quickstart-settings'));
+		setupColorInputs();
+		settings.load('quickstart', $('.quickstart-settings'));
+		$('#save').on('click', saveSettings);
+	};
 
-		$('#save').on('click', function () {
-			Settings.save('quickstart', $('.quickstart-settings'), function () {
-				app.alert({
-					type: 'success',
-					alert_id: 'quickstart-saved',
-					title: 'Settings Saved',
-					message: 'Please reload your NodeBB to apply these settings',
-					clickfn: function () {
-						socket.emit('admin.reload');
-					},
-				});
+	function saveSettings() {
+		settings.save('quickstart', $('.quickstart-settings'), function () {
+			app.alert({
+				type: 'success',
+				alert_id: 'quickstart-saved',
+				title: 'Settings Saved',
+				message: 'Please reload your NodeBB to apply these settings',
+				clickfn: function () {
+					socket.emit('admin.reload');
+				},
 			});
 		});
-	};
+	}
+
+	function setupColorInputs() {
+		var colorInputs = $('[data-settings="colorpicker"]');
+		colorpicker.enable(colorInputs, updateColors);
+		colorInputs.on('change', updateColors);
+		updateColors();
+	}
+
+	function updateColors() {
+		$('#preview').css({
+			color: $('#color').val(),
+			'background-color': $('#bgColor').val(),
+		});
+	}
 
 	return ACP;
 });
--- static/templates/admin/plugins/quickstart.tpl
+++ static/templates/admin/plugins/quickstart.tpl
@@ -16,6 +16,23 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="row">
+		<div class="col-sm-2 col-xs-12 settings-header">Colors</div>
+		<div class="col-sm-10 col-xs-12">
+			<p class="alert" id="preview">
+				Here is some preview text. Use the inputs below to modify this alert's appearance. 
+			</p>
+			<div class="form-group">
+				<label for="color">Foreground</label>
+				<input data-settings="colorpicker" type="text" id="color" name="color" title="Background Color" class="form-control" placeholder="#ffffff" value="#ffffff" />
+			</div>
+			<div class="form-group">
+				<label for="bgColor">Background</label>
+				<input data-settings="colorpicker" type="text" id="bgColor" name="bgColor" title="Background Color" class="form-control" placeholder="#000000" value="#000000" />
+			</div>
+		</div>
+	</div>
 </form>
 
 <button id="save" class="floating-button mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
