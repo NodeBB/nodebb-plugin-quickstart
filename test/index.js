ba95cc448d85d037a67523883be24485bc49be37
Julian Lam <julian@nodebb.org>
2021-09-14 16:31:05 -0400

    style: lint test/index.js

--- test/index.js
+++ test/index.js
@@ -1,20 +1,23 @@
 // running "npm test" will run tests in this file
 // to run only plugin tests use "mocha test/plugins-installed.js"
+
 'use strict';
+
+/* globals describe, it */
 
 const assert = require('assert');
 
 const db = require.main.require('./test/mocks/databasemock');
 
-describe('nodebb-plugin-quickstart', function () {
-	it('should pass', function (done) {
+describe('nodebb-plugin-quickstart', () => {
+	it('should pass', (done) => {
 		const actual = 'value';
 		const expected = 'value';
 		assert.strictEqual(actual, expected);
 		done();
 	});
 
-	it('should load config object', async function () {
+	it('should load config object', async () => {
 		const config = await db.getObject('config');
 		assert(config);
 	});
