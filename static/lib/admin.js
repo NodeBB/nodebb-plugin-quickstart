92c705eee2d39a59b26a4c73652d73f14a26d8fd
Julian Lam <julian@nodebb.org>
2023-08-04 16:21:10 -0400

    refactor: quickstart acp client-side js to ES Module

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -5,52 +5,51 @@
 	It is only loaded when the user navigates to /admin/plugins/quickstart page
 	It is not bundled into the min file that is served on the first load of the page.
 */
-define('admin/plugins/quickstart', [
-	'settings', 'uploader',
-], function (settings, uploader) {
-	var ACP = {};
 
-	ACP.init = function () {
-		setupUploader();
-		settings.load('quickstart', $('.quickstart-settings'), function () {
-			setupColorInputs();
-		});
-		$('#save').on('click', saveSettings);
-	};
+import { save, load } from 'settings';
+import * as uploader from 'uploader';
 
-	function saveSettings() {
-		settings.save('quickstart', $('.quickstart-settings')); // pass in a function in the 3rd parameter to override the default success/failure handler
-	}
+export function init() {
+	handleSettingsForm();
+	setupUploader();
+};
 
-	function setupColorInputs() {
-		var colorInputs = $('[data-settings="colorpicker"]');
-		colorInputs.on('change', updateColors);
-		updateColors();
-	}
+function handleSettingsForm() {
+	load('quickstart', $('.quickstart-settings'), function () {
+		setupColorInputs();
+	});
 
-	function updateColors() {
-		$('#preview').css({
-			color: $('#color').val(),
-			'background-color': $('#bgColor').val(),
-		});
-	}
+	$('#save').on('click', () => {
+		save('quickstart', $('.quickstart-settings')); // pass in a function in the 3rd parameter to override the default success/failure handler
+	});
+}
 
-	function setupUploader() {
-		$('#content input[data-action="upload"]').each(function () {
-			var uploadBtn = $(this);
-			uploadBtn.on('click', function () {
-				uploader.show({
-					route: config.relative_path + '/api/admin/upload/file',
-					params: {
-						folder: 'quickstart',
-					},
-					accept: 'image/*',
-				}, function (image) {
-					$('#' + uploadBtn.attr('data-target')).val(image);
-				});
+function setupColorInputs() {
+	var colorInputs = $('[data-settings="colorpicker"]');
+	colorInputs.on('change', updateColors);
+	updateColors();
+}
+
+function updateColors() {
+	$('#preview').css({
+		color: $('#color').val(),
+		'background-color': $('#bgColor').val(),
+	});
+}
+
+function setupUploader() {
+	$('#content input[data-action="upload"]').each(function () {
+		var uploadBtn = $(this);
+		uploadBtn.on('click', function () {
+			uploader.show({
+				route: config.relative_path + '/api/admin/upload/file',
+				params: {
+					folder: 'quickstart',
+				},
+				accept: 'image/*',
+			}, function (image) {
+				$('#' + uploadBtn.attr('data-target')).val(image);
 			});
 		});
-	}
-
-	return ACP;
-});
+	});
+}
